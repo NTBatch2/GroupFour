11714c589c4d58b870a9b43dc257d5024543f2fd
solaiman_98 <ahmadsolaimanahadi2.0@gmail.com>
2020-05-16 16:00:21 -0400

    done by Solaiman

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -4,12 +4,10 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Group Four");
-		//Sergii comment..
 	}
 	
-	public void partTwo() {
-		System.out.println("Learning Github");
+	public void second() {
+		System.out.println("Second Stash");
 	}
-	
 	
 }
