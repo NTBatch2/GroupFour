73e9cb6548a5874e9e9aec80102c6d523d9f1775
noort <noort@10.1.10.35>
2020-05-17 11:24:30 -0400

    removed methods from SaraClass

--- src/noorteck/SaraClass.java
+++ src/noorteck/SaraClass.java
@@ -1,19 +1,5 @@
 package noorteck;
 
 public class SaraClass {
-	public static void main(String[] args) {
-		System.out.println("Please work");
-		System.out.println("Please work");
-	}
-	//New comment for practicing
-
-	
-	public void display() {
-		System.out.println("More Practice");
-	}
-	
-	public void div() {
-		System.out.println("div");
-	}
-	//added comment
+	 
 }
