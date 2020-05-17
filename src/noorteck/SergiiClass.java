d6d36f1043b4bdc0ff26638adcf0c8c41882e4a9
bcade <bcade@LAPTOP-7OSUL94B.fios-router.home>
2020-05-17 01:40:46 -0400

    Trying to get much practice

--- src/noorteck/SergiiClass.java
+++ src/noorteck/SergiiClass.java
@@ -4,5 +4,12 @@ public class SergiiClass {
 
 	public static void main(String[] args) {
 		System.out.println("How are you guys doing?");
+		
+		/**how come I can add comments but not a new class?
+		 * 
+		 * after I stash the class I get error message saying
+		 * Does not Contain any local changes
+		 * what is that?? do we saved class differently then comments?
+		*/
 	}
 }
